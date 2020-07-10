fca0c47a600cd15ab0a6c735adf8881f8a9541f0
Steven McAllister <steved1mac@gmail.com>
2020-07-10 21:16:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ game_hash = {
   :home => {
     :team_name => "Brooklyn Nets",
     :colors => ["black", "white"], 
-    :players => [{:Player_name =>"Alan Anderson", :Shoe => 16, :Points => 22, :Rebounds => 12, :Assists => 12, :Steals => 3, :Blocks => 1, :Slam_dunks => 1}, {:Player_name =>"Reggie Evans", :Number => 30, :Shoe => 14, :Points => 12, :Rebounds => 12, :Assists => 12, :Steals => 12, :Blocks => 12, :Slam_dunks => 7},{:Player_name =>"Brook Lopez",:Number => 11, :Shoe => 17, :Points => 17, :Rebounds => 19, :Assists => 10, :Steals => 3, :Blocks => 1, :Slam_dunks => 15},  {:Player_name =>"Mason Pumlee", :Shoe => 19, :Points => 26, :Rebounds => 11, :Assists => 6, :Steals => 3, :Blocks => 8, :Slam_dunks => 5}, {:Player_name => "Jason Terry", :Number => 31, :Shoe => 15, :Points => 19, :Rebounds => 2, :Assists => 2, :Steals => 4, :Blocks => 11, :Slam_dunks => 1}], }
+    :players => [{:Player_name =>"Alan Anderson", :Shoe => 16, :Points => 22, :Rebounds => 12, :Assists => 12, :Steals => 3, :Blocks => 1, :Slam_dunks => 1}, {:Player_name =>"Reggie Evans", :Number => 30, :Shoe => 14, :Points => 12, :Rebounds => 12, :Assists => 12, :Steals => 12, :Blocks => 12, :Slam_dunks => 7},{:Player_name =>"Brook Lopez",:Number => 11, :Shoe => 17, :Points => 17, :Rebounds => 19, :Assists => 10, :Steals => 3, :Blocks => 1, :Slam_dunks => 15},  {:Player_name =>"Mason Pumlee", :Shoe => 19, :Points => 26, :Rebounds => 11, :Assists => 6, :Steals => 3, :Blocks => 8, :Slam_dunks => 5}, {:Player_name => "Jason Terry", :Number => 31, :Shoe => 15, :Points => 19, :Rebounds => 2, :Assists => 2, :Steals => 4, :Blocks => 11, :Slam_dunks => 1}}]
   
   
   :away => {
