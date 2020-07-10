f3cf94c7efefaf5d2cb34123bdfb99080ddeb5c3
Steven McAllister <steved1mac@gmail.com>
2020-07-10 21:49:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,8 @@ def game_hash
     :players => [
       {
         :player_name =>"Alan Anderson", 
-      :shoe => 16, 
+      :number => 
+      :shoe => 16,
       :points => 22, 
       :rebounds => 12,
       :assists => 12, 
