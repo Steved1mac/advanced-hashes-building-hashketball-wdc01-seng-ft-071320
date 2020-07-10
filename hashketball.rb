bacb63a841f5ff608b1f0f8feb31f2c0ad96c6a5
Steven McAllister <steved1mac@gmail.com>
2020-07-10 21:17:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,7 +9,7 @@ game_hash = {
   :away => {
     :team_name => "Charlotte Hornets", 
     :colors => ["Turquoise", "Purple"],
-    :players => [{:Player_name =>"Jeff Adrien", :Shoe => 18, :Points: 10, :Rebounds => 1, :Assists => 1, :Steals => 2, :Blocks => 7, :Slam_dunks => 2}, :Player_name =>"Bismack Biyombo", :Number => 0, :Shoe => 16, :Points => 12, :Rebounds => 4, :Assists => 7, :Steals => 22, :Blocks => 15, :Slam_dunks => 10}, {:Player_name =>"DeSagna Diop", :Shoe => 14, :Points => 24, :Rebounds => 12, :Assists => 12, :Steals => 4, :Blocks => 5, :Slam_dunks => 5}, {:Player_name =>"Ben Gordan", :Number => 8, :Shoe => 15, :Points=> 33, :Rebounds => 3, :Assists => 2, :Steals => 1, :Blocks => 5, :Slam_dunks => 12}, {:Player_name =>"Kemba Walker", :Number => 33, :Shoe => 15, :Points => 6, :Rebounds => 12, :Assists => 12, :Steals => 7, :Bl
+    :players => [{:Player_name =>"Jeff Adrien", :Shoe => 18, :Points: 10, :Rebounds => 1, :Assists => 1, :Steals => 2, :Blocks => 7, :Slam_dunks => 2}, :Player_name =>"Bismack Biyombo", :Number => 0, :Shoe => 16, :Points => 12, :Rebounds => 4, :Assists => 7, :Steals => 22, :Blocks => 15, :Slam_dunks => 10}, {:Player_name =>"DeSagna Diop", :Shoe => 14, :Points => 24, :Rebounds => 12, :Assists => 12, :Steals => 4, :Blocks => 5, :Slam_dunks => 5}, {:Player_name =>"Ben Gordan", :Number => 8, :Shoe => 15, :Points=> 33, :Rebounds => 3, :Assists => 2, :Steals => 1, :Blocks => 5, :Slam_dunks => 12}, {:Player_name =>"Kemba Walker", :Number => 33, :Shoe => 15, :Points => 6, :Rebounds => 12, :Assists => 12, :Steals => 7, 
 :Blocks => 5, :Slam_dunks => 12}]
   }}
   
